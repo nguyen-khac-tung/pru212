d4ad2466051e7dfece673c8b8a3154818c6472a4
nguyenkhactung <khactung1611@gmail.com>
2025-01-15 23:48:35 +0700

    HttpClient

---
+++ DemoHttpClient/HttpClientApp/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HttpClientApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly HttpClient client = new HttpClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnViewHTML_Click(object sender, RoutedEventArgs e)
+        {
+            string uri = txtURL.Text;
+            try
+            {
+                string responseBody = await client.GetStringAsync(uri);
+                txtContent.Text = responseBody.Trim();
+            }
+            catch (HttpRequestException ex)
+            {
+                MessageBox.Show($"Message :{ex.Message}");
+            }
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            txtContent.Text = string.Empty;
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
